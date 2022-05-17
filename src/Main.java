6f57b5f1ad376f2acd7391482bc852b28379fb68
Ilya Kurbatov <ilya-balon@mail.ru>
2022-05-17 17:25:55 +1000

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите адрес электронной почты: ");
+        String email = scan.nextLine();
+        String[] domain = email.split("@");
+        System.out.println(domain[1]);
+    }
+}
